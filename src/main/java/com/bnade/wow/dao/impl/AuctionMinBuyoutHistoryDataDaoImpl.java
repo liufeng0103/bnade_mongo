49e1a4b77c2d0cd695aaef1923f5cb36bc1d8e66
Feng Liu <liufeng0103@163.com>
2016-05-04 14:15:10 +0800

    历史集合添加索引
    每次保存数据时检查历史集合是否有索引，没有就创建

--- src/main/java/com/bnade/wow/dao/impl/AuctionMinBuyoutHistoryDataDaoImpl.java
+++ src/main/java/com/bnade/wow/dao/impl/AuctionMinBuyoutHistoryDataDaoImpl.java
@@ -4,21 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.bnade.util.MongoUtil;
 import com.bnade.wow.dao.AuctionMinBuyoutHistoryDataDao;
 import com.bnade.wow.po.HistoryAuction;
 import com.mongodb.Block;
 import com.mongodb.client.FindIterable;
+import com.mongodb.client.ListIndexesIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.IndexModel;
 
 public class AuctionMinBuyoutHistoryDataDaoImpl implements AuctionMinBuyoutHistoryDataDao {
+	
+	private static Logger logger = LoggerFactory.getLogger(AuctionMinBuyoutHistoryDataDaoImpl.class);
 
 	private static final String COLLECTION_NAME_PREFIX = "historyAuction_";
+	private static final List<IndexModel> INDEXES = new ArrayList<>();	
+	
+	static {
+		// 查询物品的所有历史记录
+		INDEXES.add(new IndexModel(new Document("item", 1).append("bonusLists", 1))); 
+	}
+	
+	private void checkAndCreateIndex(MongoCollection<Document> coll) {
+		ListIndexesIterable<Document> indexes = coll.listIndexes();
+		if(!indexes.iterator().hasNext()) {
+			logger.info("索引未创建,为集合{}创建索引", coll.getNamespace().getCollectionName());
+			coll.createIndexes(INDEXES);
+		}		
+	}
 	
 	@Override
 	public void save(List<HistoryAuction> aucs, int realmId, int year) {
 		MongoCollection<Document> coll = MongoUtil.getWowDB().getCollection(COLLECTION_NAME_PREFIX + year + realmId);
+		checkAndCreateIndex(coll);
 		List<Document> docs = new ArrayList<Document>();
 		for (HistoryAuction auc : aucs) {
 			Document doc = new Document("item", auc.getItem())
